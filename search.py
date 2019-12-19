3eab4df2538d511e770d4ec44d0be218537a380a
Jbqiangqiangqiang <58978042+Jbqiangqiangqiang@users.noreply.github.com>
2019-12-19 16:02:28 +0800

    Update search.py

--- search.py
+++ search.py
@@ -11,6 +11,7 @@ def non_star_search(name):
     for star in main.star_relationships:
         star_relationship=star_relationships[star]
         if name in star_relationship.values():
+            relationship=list(star_relationship.keys())[list(star_relationship.values()).index(name)]
             print(name,"是",star,"的",relationship)
 
 
