346489c846afcfd7502c06d44d4cc36f211da3d3
Jbqiangqiangqiang <58978042+Jbqiangqiangqiang@users.noreply.github.com>
2019-12-19 20:54:15 +0800

    fix: search.py
    Added the function to check without this person

--- search.py
+++ search.py
@@ -6,10 +6,15 @@
 # Author:jbqiangqiangqiang
 
 def non_star_search(name,star_relationships):
+    tage=0
     for star in star_relationships:
         star_relationship=star_relationships[star]
         for relationship in star_relationship:
             if name==relationship[1]:
                 print("{}是{}的{}".format(name,star,relationship[0]))
+                tage+=1
+    if not tage:
+        print("查无此人")
 
+     
 
