20ed0f4073fb2611b047517712993a6ddf1f35a4
Jbqiangqiangqiang <58978042+Jbqiangqiangqiang@users.noreply.github.com>
2019-12-19 15:57:45 +0800

    feature: new search function
    search function supplement

--- search.py
+++ search.py
@@ -11,7 +11,6 @@ def non_star_search(name):
     for star in main.star_relationships:
         star_relationship=star_relationships[star]
         if name in star_relationship.values():
-            relationship=list(a.keys())[list(a.values()).index(name)]
             print(name,"是",star,"的",relationship)
 
 
