4d92c1fea52eac054f2563c11c65b5e1b3bfdbdd
ericyang7816 <ericyang7816@foxmail.com>
2019-12-21 21:23:11 +0800

    refactor: all
    new docstring
    new exception handling in main.py
    bug fix
    new style

--- main.py
+++ main.py
@@ -3,23 +3,22 @@
 
 'main scrap program'
 
-# Author: ericyang pigxx500kg jbqiangqiangqiang
+# Author: ericyang7816 pigxx500kg jbqiangqiangqiang
 
 from urllib import parse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 import graph
-import read
 import search
 
-star_relationships = {}
-pages = []
-MAX_TRY = 6
+star_relationships = {}  # 人物的关系
+pages = []  # 查询的页面
+MAX_TRY = 8  # 查询深度，会遍历 MAX_TRY-1 个人物
 
 
 name = input("请输入你要查询的明星：")
-url = 'https://baike.baidu.com/item/'+parse.quote_plus(name)
+url = 'https://baike.baidu.com/item/'+parse.quote_plus(name)  # 将中文名进行转义
 
 
 def find_star(url):
@@ -31,17 +30,18 @@ def find_star(url):
     else:
         star_relation_list = []  # 明星关系总列表
         pages.append(url)  # 加入到总的pages列表
-        html = urlopen(url)
-
+        try:
+            html = urlopen(url)
+        except Exception:
+            return
         bsObj = BeautifulSoup(html, 'html.parser')
-
         star_name = bsObj.find(
-            'dd', {"class": "lemmaWgt-lemmaTitle-title"}).h1.get_text()    # 明星的名字(str)
+            'dd', {"class": "lemmaWgt-lemmaTitle-title"}).h1.get_text()    # 查找明星的名字
         try:
             star_list = bsObj.find(
                 "div", {"class": "viewport", "id": "slider_relations"}).find_all("li")
         except Exception:
-            return
+            return  # 如果当前人物不存在关系，就返回继续查找
         else:
             MAX_TRY = MAX_TRY - 1
         star_list = bsObj.find(
@@ -49,14 +49,15 @@ def find_star(url):
         url_list = bsObj.find(
             "div", {"class": "viewport", "id": "slider_relations"}).find_all("a")
         for star in star_list:
-            name = star.find("div", {"class": "name"}).em.get_text()  # 名字
+            name = star.find("div", {"class": "name"}).em.get_text()  # 获取姓名
             lenth1 = len(name)
-            relationship = star.find("div", {"class": "name"}).get_text()  # 关系+名字
+            relationship = star.find(
+                "div", {"class": "name"}).get_text()  # 返回关系和姓名的组合
             lenth2 = len(relationship)
             lenth = lenth2 - lenth1
-            one_relation = [relationship[0:lenth],name]
+            one_relation = [relationship[0:lenth], name]  # 利用切片将关系提取
             star_relation_list.append(one_relation)
-        star_relationships[star_name] = star_relation_list       # 将明星关系列表加入总字典
+        star_relationships[star_name] = star_relation_list  # 将明星关系列表加入总字典
         for url in url_list:
             if "href" in url.attrs:
                 new_page = "https://baike.baidu.com"+url.attrs["href"]
@@ -66,17 +67,15 @@ def find_star(url):
 
 
 find_star(url)
-
-print(star_relationships)
-
-
-
-graph.graph_generate(star_relationships)
-while True:
-    name=input('请输入要查询的人物: ')
-    print()
-    if name in star_relationships.keys():
-        read.read_one_star(name,star_relationships)
-    else:
-        search.non_star_search(name,star_relationships)
-
+if star_relationships: #当查找到人物关系时，向下执行
+    graph.graph_generate(star_relationships)  # 生成关系图
+    while True:
+        name = input('请输入要查询的人物: ')
+        print()
+        # 如果没有遍历过要查询的人物的关系，就以其他人为中心查找TA，否则返回以TA为中心TA和其他人的关系
+        if name in star_relationships.keys():
+            search.center_search(name, star_relationships)
+        else:
+            search.non_center_search(name, star_relationships)
+else:
+    print('未找到人物关系...')
--- search.py
+++ search.py
@@ -1,23 +1,48 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-'search function supplement'
+'Functions to find the relationship between figures.'
 
-# Author:jbqiangqiangqiang
+# Author: jbqiangqiangqiang liuwx1015 ericyang7816
 
-def non_star_search(name,star_relationships):
-    tag=0
+
+def non_center_search(name, star_relationships):
+    '''Show the relationships of a person who is not the center of a relationship.'''
+    tag = 0
     print(name+"的关系情况：")
     print('----------------')
     for star in star_relationships:
-        star_relationship=star_relationships[star]
+        star_relationship = star_relationships[star]
         for relationship in star_relationship:
-            if name==relationship[1]:
-                print("{}是{}的{}".format(name,star,relationship[0]))
-                tag+=1
-    print('----------------')
+            if name == relationship[1]:  # 使用format格式化字符串方便输出
+                print("{}是{}的{}".format(name, star, relationship[0])) 
+                tag = 1
     if not tag:
-        print("查无此人")
+        print("Error: 查无此人")
+    print('----------------')
+    print('\n')
+    
 
-     
 
+def center_search(name, star_relationships):
+    '''Show the relationships of a person who is the center of a relationship.'''
+    if name not in star_relationships.keys():
+        return 0
+    print(name+"的关系情况：")
+    print('----------------')
+    relationships = star_relationships[name]
+    new_dic = {}
+    for relationship_and_name in relationships:
+        relationship = relationship_and_name[0]
+        name_list = [relationship_and_name[1]]  # 将姓名存储到列表中
+        if relationship in new_dic.keys():  # 当关系已存在时，将已存在的人加入到列表中
+            exist_name_list = new_dic[relationship]
+            name_list = exist_name_list+name_list
+        new_dic[relationship] = name_list
+    for relationship in new_dic:
+        print(relationship+": ", end="")
+        for s_name in new_dic[relationship]:
+            print(s_name+" ", end="")
+        print()
+    print('----------------')
+    print('\n')
